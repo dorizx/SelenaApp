a239f8511f916ef3078d1cccd2cbfdb8ff8fe5e5
aditya daffa syahputra <adityadaffasyahputra@gmail.com>
2024-11-17 00:12:53 +0700

    update layout

--- app/src/main/java/com/example/selenaapp/ui/transaction/FormAddTransactionActivity.kt
+++ app/src/main/java/com/example/selenaapp/ui/transaction/FormAddTransactionActivity.kt
@@ -1,21 +1,26 @@
 package com.example.selenaapp.ui.transaction
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.selenaapp.R
+import com.example.selenaapp.databinding.ActivityFormAddTransactionBinding
 
 class FormAddTransactionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormAddTransactionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityFormAddTransactionBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_form_add_transaction)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val transactionTypes = arrayOf("Pendapatan", "Penjualan")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, transactionTypes)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.typeDropdown.adapter = adapter
     }
 }
