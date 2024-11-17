a51fc5cdda2e4abd77f21d0bf322be5f790ecc1a
aditya daffa syahputra <adityadaffasyahputra@gmail.com>
2024-11-17 21:20:10 +0700

    add login activity

---
+++ app/src/main/java/com/example/selenaapp/ui/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.selenaapp.ui.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.selenaapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
