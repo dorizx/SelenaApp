5780f0aa140bdf77200e807f6bc34a6f8ff569e2
aditya daffa syahputra <adityadaffasyahputra@gmail.com>
2024-11-28 19:46:14 +0700

    update transaction page

--- app/src/main/java/com/example/selenaapp/ui/transaction/TransactionFragment.kt
+++ app/src/main/java/com/example/selenaapp/ui/transaction/TransactionFragment.kt
@@ -23,16 +23,12 @@ class TransactionFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
+        val transactionViewModel =
             ViewModelProvider(this).get(TransactionViewModel::class.java)
 
         _binding = FragmentTransactionBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
