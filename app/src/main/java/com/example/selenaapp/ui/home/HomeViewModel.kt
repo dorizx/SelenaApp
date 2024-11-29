4704e4e4595f7e4826aefcf8266e815312949ee8
aditya daffa syahputra <adityadaffasyahputra@gmail.com>
2024-11-29 10:57:26 +0700

    Update layout

--- app/src/main/java/com/example/selenaapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/selenaapp/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Ini Beranda"
+        value = "Testing"
     }
     val text: LiveData<String> = _text
 }
