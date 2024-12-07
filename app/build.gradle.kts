d0f563658a7bbebfe1cc397ff1077e0e5dc10bef
unknown <ajengpuspaandini04@gmail.com>
2024-12-08 02:26:59 +0700

    update detail

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
