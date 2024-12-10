c279ea7d7491bc08dff2b7ded131143281439a98
unknown <ajengpuspaandini04@gmail.com>
2024-12-10 16:26:15 +0700

    add forgetPassword

--- app/src/main/java/com/example/selenaapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/selenaapp/ui/login/LoginActivity.kt
@@ -31,6 +31,9 @@ class LoginActivity : AppCompatActivity() {
         binding.signupButton.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))
         }
+        binding.forgotPasswordTextView.setOnClickListener {
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
+        }
 
         supportActionBar?.hide()
 
